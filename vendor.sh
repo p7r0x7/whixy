3f8828fe0d136da281f88e3e163dabc6e859b61c
Matt R Bonnette <mattrbonnette@pm.me>
2024-09-16 21:28:44 -0400

    vendor corrections.

--- vendor.sh
+++ vendor.sh
@@ -31,8 +31,7 @@ llvm()
 	semv=18.1.8 deps="clang cmake compiler-rt libunwind lld llvm polly runtimes third-party"
 	url="https://github.com/llvm/llvm-project/releases/download/llvmorg-$semv"; base="llvm-project-$semv.src.tar.xz"
 	dl 450adbb7590dda12a622cb3d74a51cb101ac7eb58177b9e25b93b2abdfc5dcdc
-	7zz e -so "$srcs/$base" | tar -xf - -C "$pkg" --strip-components=1 \
-		$(for dep in $deps; do printf "llvm-project-%s.src/%s\n" "$semv" "$dep"; done)
+	7zz e -so "$srcs/$base" | tar -xf - --strip-components=1 -C "$pkg" $(printf "llvm-project-$semv.src/%s\n" $deps)
 	(
 		cd "$pkg"
 		for dep in $deps; do mv "$dep" "$dep-$semv"; done
@@ -43,7 +42,7 @@ llvm()
 
 common()
 {
-	start common aca5484881e028b2a278b7025616f5fc462ceb52472e355d46dbee45f96931a9
+	start common 37579a5ceee6bd3fc1a6ad89174948bdc5dccdc2f1d7ef49d1c820ee8a11a5a6
 
 	semv=4.13.2; base="antlr-$semv-complete.jar"; url=https://www.antlr.org/download
 	{
@@ -55,7 +54,7 @@ common()
 		dl 0fac612afb44eb1e188f4de50f00ffd4972022480084cc6afb4d9222244aef6c
 		install "$pkg/antlr-cpp-runtime-$semv"; 7zz x "$srcs/$base" -o"$pkg/antlr-cpp-runtime-$semv" >/dev/null
 
-		rm -rf "$vendor/antlr-cpp-runtime-$semv/demo"
+		rm -rf "$pkg/antlr-cpp-runtime-$semv/demo"
 	} &
 
 	semv=1.5.6; base="zstd-$semv.tar.zst"; url="https://github.com/facebook/zstd/releases/download/v$semv"
@@ -63,7 +62,8 @@ common()
 		dl eaec93bd5737d25a816d33f0cee57443230f0ecc980f0eb0573602239f3e484e
 		install "$pkg/zstd-$semv"; zstd -cd "$srcs/$base" | tar -xf - --strip-components=1 -C "$pkg/zstd-$semv"
 
-		cd "$pkg/zstd-$semv"; rm -rf contrib doc examples programs zlibWrapper lib/legacy tests
+		cd "$pkg/zstd-$semv"; rm -rf contrib doc programs zlibWrapper lib/legacy tests
+		find . -type d -name examples -exec rm -rf {} +
 	} &
 
 	wait && finish
@@ -93,13 +93,13 @@ finish()
 	b3sum "$tar"; zstd "$tar" -o "$tzst"; zstd -lv "$tzst"; mv "$tzst" .
 }
 
-set -e; umask 0022; srcs=/tmp/srcs; command -v gtar >/dev/null && tar() { command gtar "$@"; }
+set -eu; umask 0022; srcs=/tmp/srcs; command -v gtar >/dev/null && tar() { gtar "$@"; }
 tar --version | awk '{exit ($4 >= 1.28) ? 0 : 1}' || { printf 'GNU tar too old.'; return 1; }
-zstd() { command zstdmt --ultra -22 --long=29 --no-check --no-progress "$@"; }
-clean() { command rm -rf "$pkg"* >/dev/null & }
+zstd() { zstdmt --ultra -22 --long=29 --no-check --no-progress "$@"; }
 install() { command install -dm 0755 "$@"; }
+clean() { rm -rf "$pkg"* >/dev/null & }
 curl() { command curl -sL "$@"; }
-[ -n "$1" ] && return 0 # Sourcing mode ends here.
+[ $# = 0 ] || return 0 # Sourcing mode ends here.
 
 for dir in "$srcs" vendor; do [ -d "$dir" ] || install "$dir"; done; cd vendor
 gcc & llvm & common & wait
